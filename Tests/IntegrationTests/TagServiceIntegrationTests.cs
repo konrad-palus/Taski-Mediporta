bb9981163e8626bc24db6a7fe003a4c0225df5dd
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-04-04 01:03:49 +0200

    Tests

---
+++ Tests/IntegrationTests/TagServiceIntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace TaskApi_MediportaTests
+{
+    public class TagServiceIntegrationTest
+    {
+        private readonly TagService _tagService;
+        private readonly IMemoryCache _memoryCache;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TagServiceIntegrationTest()
+        {
+            _httpClientFactory = new HttpClientFactoryStub();
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
+            _tagService = new TagService(_httpClientFactory, _memoryCache, new NullLogger<TagService>());
+        }
+
+        [Fact]
+        public async Task ImportTagsAsync_TagsImportedSuccessfully()
+        {
+            // Act
+            await _tagService.ImportTagsAsync();
+
+            // Assert
+            var tagsExistInCache = _memoryCache.TryGetValue("TagsList", out object tags);
+            Assert.True(tagsExistInCache, "Tags were not imported successfully.");
+            Assert.NotNull(tags);
+        }
+    }
+
+    public class HttpClientFactoryStub : IHttpClientFactory
+    {
+        public HttpClient CreateClient(string name = null)
+        {
+            return new HttpClient();
+        }
+    }
+}
---
+++ Tests/UnitTests/TagServiceUnitTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using TaskApi_Mediporta.Models;
+using Xunit;
+namespace TaskApi_Mediporta.UnitTests
+{
+    public class TagServiceUnitTests
+    {
+        [Fact]
+        public void GetPaginatedTags_ReturnsExpectedTag()
+        {
+            // Arrange
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var tags = new List<Tag>
+            {
+                new Tag { Name = "C#", Count = 2 },
+                new Tag { Name = "Java", Count = 2 },
+                new Tag { Name = "Sqlite", Count = 200 },
+            };
+
+            memoryCache.Set("TagsList", tags);
+
+            var loggerMock = new Mock<ILogger<TagService>>();
+
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            var httpClientMock = new Mock<HttpClient>();
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClientMock.Object);
+
+            var service = new TagService(httpClientFactoryMock.Object, memoryCache, loggerMock.Object);
+
+            var requestModel = new GetTagsRequestModel
+            {
+                PageNumber = 1,
+                PageSize = 1,
+                SortBy = SortByEnum.Name,
+                SortOrder = SortOrderEnum.Desc
+            };
+
+            // Act
+            var result = service.GetPaginatedTags(requestModel);
+
+            // Assert
+            Assert.Single(result.Tags);
+            Assert.Equal("Sqlite", result.Tags.First().Name);
+        }
+
+        [Fact]
+        public void GetPaginatedTags_WhenCacheIsEmpty_ReturnsEmptyTagsList()
+        {
+            // Arrange
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+            var loggerMock = new Mock<ILogger<TagService>>();
+
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            var httpClientMock = new Mock<HttpClient>();
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClientMock.Object);
+
+            var service = new TagService(httpClientFactoryMock.Object, memoryCache, loggerMock.Object);
+
+            var requestModel = new GetTagsRequestModel
+            {
+                PageNumber = 1,
+                PageSize = 1,
+                SortBy = SortByEnum.Name,
+                SortOrder = SortOrderEnum.Asc
+            };
+
+            // Act
+            var result = service.GetPaginatedTags(requestModel);
+
+            // Assert
+            Assert.Empty(result.Tags);
+        }
+    }
+}
